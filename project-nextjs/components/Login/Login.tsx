a79e60fc6fd9c12bca00e038a7d370cc4347a189
ulises2125 <ulisesresumenes@gmail.com>
2023-04-15 00:13:22 -0300

    css login

--- project-nextjs/components/Login/Login.tsx
+++ project-nextjs/components/Login/Login.tsx
@@ -1,12 +1,64 @@
-import "./login.css";
+import { useState } from "react";
+import styles from "./login.module.css";
 
-const Login = () => (
-  <>
-    <div className="div-general">
-      <div className="div-left">LEFT</div>
-      <div className="div-right">RIGTH</div>
-    </div>
-  </>
-);
+interface Idata {
+  email: string;
+  password: string;
+}
+
+const Login = () => {
+  const [disable, setDisable] = useState<boolean>(false);
+  const [data, setData] = useState<Idata>({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <div className={styles.divGeneral}>
+        <div className={styles.divLeft}>
+          <div className={styles.center}>
+            <h1>Logo</h1>
+            <h2>Regístrate para apoyar </h2>
+          </div>
+        </div>
+        <div className={styles.divRight}>
+          <div className={styles.center}>
+            <div className={styles.cabezal}>
+              <h1>Logo</h1>
+              <h2>Regístrate para apoyar </h2>
+            </div>
+            <span>Iniciar sesión</span>
+            <form onSubmit={handleSubmit}>
+              <input
+                name="email"
+                className={styles.input}
+                placeholder="Correo"
+              />
+              <input
+                name="password"
+                className={styles.input}
+                placeholder="Contraseña"
+              />
+              <button type="submit" className={styles.button}>
+                Iniciar sesión
+              </button>
+              <button className={styles.buttonL}>
+                ¿Has olvidado la contraseña?
+              </button>
+              <button disabled={disable} className={styles.buttonL}>
+                Regístrate
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
